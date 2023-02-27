5d89b7381039aeec87eb1b61ee9333621fb3af96
Maxdeviations <maks.manin.2020@mail.ru>
2023-02-27 17:22:20 +0300

    Add readme and class main

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,8 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        int[] sales = new int[] {6, 4, 3};
+        SalesManager salesManager2 = new SalesManager(sales);
+        int max = salesManager2.max();
+        System.out.println(max);
     }
 }
