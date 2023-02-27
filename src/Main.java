081b7796ea8c12af6e8d57ac4375f8698fa4137e
Maxdeviations <maks.manin.2020@mail.ru>
2023-02-27 18:50:45 +0300

    Add min() and average() methods in SalesManager class

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,10 @@ public class Main {
         int[] sales = new int[] {6, 4, 3};
         SalesManager salesManager2 = new SalesManager(sales);
         int max = salesManager2.max();
+        int min = salesManager2.min();
+        int average = salesManager2.average();
         System.out.println(max);
+        System.out.println(min);
+        System.out.println(average);
     }
 }
